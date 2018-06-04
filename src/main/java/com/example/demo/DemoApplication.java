2929d3dbb473766ba984437c796eae031cfb2ae4
laojiaqi <lywhlao@qq.com>
2018-06-04 15:49:12 +0800

    feat:add Eureka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
+@EnableEurekaServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
